1710bdfbcba183d020f3a99ecdd541d997be9549
ROUDAUT Florian <floflo22001@outlook.fr>
2024-01-22 16:05:15 +0100

    exo3 tp2

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from datetime import datetime
 import pytz
+import logging
+
+
 
 timezone_paris = pytz.timezone('Europe/Paris')
 timezone_reunion = pytz.timezone('Indian/Reunion')
 
+
 def get_date_formatted(timezone):
+    logging.basicConfig(filename=None, encoding='utf-8', level=logging.DEBUG)
+    logging.info(f"Lancement du traitement")
+    logging.debug(f"Demande d'heure sur le timezone : {timezone}")
+    
+    if timezone is None:
+
+        logging.error("aucune timezone n'a été renseignée")
+
+        raise ValueError("aucune timezone n'a été renseignée")
+
     date = datetime.now(timezone)
     return date.strftime("%H:%M:%S")
+
 
 print(get_date_formatted(timezone_paris))
 print(get_date_formatted(timezone_reunion))
